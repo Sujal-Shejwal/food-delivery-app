1c986d12a728a1051ec2ac6af923b703276f8a0b
Sujal-Shejwal <sujalshejwal03@gmail.com>
2026-03-25 07:50:04 +0530

    fix form-data parsing

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRoute.js"; // ✅ FIXED
+import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
@@ -13,6 +13,7 @@ const port = 4000;
 
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // ✅ FIX
 app.use(cors());
 
 // db connection
@@ -22,7 +23,7 @@ connectDB();
 app.use("/api/food", foodRouter);
 app.use("/images", express.static('uploads'));
 app.use("/api/user", userRouter);
-app.use("/api/cart", cartRouter);   
+app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
